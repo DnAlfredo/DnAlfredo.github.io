cd20c25ca71c159132e57981b7f93bec8edbdbd8
Alfred <alfredmp6@hotmail.com>
2022-07-15 12:02:34 +0200

    para no perder el tiempo en practicas

--- jsadec.js
+++ jsadec.js
@@ -1,8 +1,68 @@
+let coleccionfotos = ['./multimediaAdec/imgabs.webp ', './multimediaAdec/imgabs2.webp', './multimediaAdec/logo-ADEC-provisional.jpg']
+let posicion = 0
+let intervalo
+let interruptor = false
+
+function muestraImg(id) {
+
+    let img = document.getElementById('img_galeria')
+    img.src = './multimediaAdec/imgabs.webp '
+    document.getElementById('divimg_galeria').style.display = "flex"
+    document.getElementById('botones_mini').style.display = "flex"
+    document.getElementById('boton_inicio').style.display = "none"
+}
+
+function cambiaImg(num) {
+    let img = document.getElementById('img_galeria')
+
+    posicion = posicion + num
+
+    if (posicion < 0) {
+        posicion = coleccionfotos.length - 1 //asi podriamos saber cuantos hay dentro del array, sacar la longitud y quitarle el de la cuenta del cero con el menos uno
+    }
+    if (posicion >= coleccionfotos.length) {
+        posicion = 0
+    }
+
+    img.src = coleccionfotos[posicion]
+}
+
+function fotoDirecta(pos) {
+    //falta poner filtros para que no tenga errores
+    let img = document.getElementById('img_galeria')
+    img.src = coleccionfotos[pos]
+    posicion = pos //para seguir con la misma caja para el resto
+}
+
 
 function carruselFotos() {
-    
- alert("mecmec")
-      
-   
-         
+    //hace falta poner un stop
+    //bajo hacemos que no le puedan dar varias veces al auto y evitar que se acumulen y exploten.De esta manera solo se acumula una vez
+    //document.getElementById("img_galeria").addEventListener("click", clearInterval(intervalo))
+
+    if (interruptor == false) {
+       
+       document.getElementById('flecha_der').style.zIndex="-3"
+       document.getElementById('icon_img1').style.zIndex="-3"
+       document.getElementById('flecha_iz').style.zIndex="-3"
+       document.getElementById('icon_img2').style.zIndex="-3"
+       document.getElementById('icon_img3').style.zIndex="-3"
+       document.getElementById('divimg_galeria').style.width="40rem"
+       intervalo = setInterval(() => {
+            cambiaImg(1)
+        }, 2000);
+        interruptor = true
+    }
+    else {
+       
+       document.getElementById('flecha_der').style.zIndex="0"
+       document.getElementById('icon_img1').style.zIndex="0"
+       document.getElementById('flecha_iz').style.zIndex="0"
+       document.getElementById('icon_img2').style.zIndex="0"
+       document.getElementById('icon_img3').style.zIndex="0"
+       document.getElementById('divimg_galeria').style.width="20rem"
+        clearInterval(intervalo)
+
+        interruptor=false
+    }
 }
